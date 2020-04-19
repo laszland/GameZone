3b50f2c935bffe4afb37c4999277a8e6da257e9f
Lacus <lacus@Lacuss-MacBook-Pro.local>
2020-04-19 23:19:10 +0200

    create screen files

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/home';
+import About from './screens/about';
+import ReviewDetails from './screens/reviewDetails';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Home/>
+      <About/>
+      <ReviewDetails/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
