ff488e1eced873f5d8920284d1a3272dbb02d07d
Laszlo Roland Kiss <laszlo.roland.kiss@gmail.com>
2020-04-21 22:27:28 +0200

    add style to stack navigation bar

---
+++ routes/homeStack.js
@@ -0,0 +1,30 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import Home from '../screens/home';
+import ReviewDetails from '../screens/reviewDetails'
+
+const screens = {
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      title: 'GameZone',
+      //headerStyle: { backgroundColor: '#bbb' }
+    }
+  },
+  ReviewDetails: {
+    screen: ReviewDetails,
+    navigationOptions: {
+      title: 'Review Details',
+      //headerStyle: { backgroundColor: '#bbb' }
+    }
+  }
+}
+
+const HomeStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    headerTintColor: '#444',
+    headerStyle: { backgroundColor: '#eee', height: 60,  }
+  }
+});
+
+export default createAppContainer(HomeStack);
