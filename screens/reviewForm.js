26e97d6a1f9c24ea4d0a78d542f84de8755afa35
Laszlo Roland Kiss <laszlo.roland.kiss@gmail.com>
2020-04-23 21:38:33 +0200

    add yup to formik form and write schema

--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import { StyleSheet, Button, Text, View, TextInput } from 'react-native';
 import { globalStyles } from '../styles/global';
-import { Formik } from 'formik';
+import { Formik, yupToFormErrors } from 'formik';
+import * as yup from 'yup';
+
+const ReviewSchema = yup.object({
+  title: yup.string()
+    .required()
+    .min(4),
+  body: yup.string()
+    .required()
+    .min(8),
+  rating: yup.string()
+    .required()
+    .test('is-num-1-5', 'Rating must be a number 1 - 5', (val) => {
+      return parseInt(val) < 6 && parseInt(val) > 0;
+    })
+})
 
 export default function ReviewForm({ addReview }) {
   return (
     <View style={globalStyles.container}>
       <Formik
         initialValues={{ title: '', body: '', rating: '' }}
+        validationSchema={ReviewSchema}
         onSubmit={(values, actions) => {
           actions.resetForm();
           addReview(values);
